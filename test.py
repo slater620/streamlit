5d694a917f6bcef796282c3d6547a2ae6b0c8dcf
slater620 <81989373+slater620@users.noreply.github.com>
2023-08-07 03:50:03 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -2,17 +2,17 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-# Chargez votre modèle entraîné
+# Charger le modèle entrainé
 model = joblib.load('model_opti.pkl')
 
-# Définissez une fonction pour prédire les faux billets
+# Fonction de prédiction et de probabilité
 def predict_fake_bill(features):
     X = pd.DataFrame([features])
-    
     prediction = model.predict(X)[0]
-    return prediction
+    proba = model.predict_proba(X)[0]
+    return prediction, proba
 
-# Créez votre interface Streamlit
+# Créer votre interface Streamlit
 def main():
     st.title('Détection de faux billet')
     st.write('Veuillez entrer les caractéristiques du billet à analyser :')
@@ -28,11 +28,13 @@ def main():
     # Bouton pour prédire
     if st.button('Prédire'):
         features = [diagonal, height_left, height_right, margin_low, margin_up, length]
-        prediction = predict_fake_bill(features)
+        prediction, proba = predict_fake_bill(features)
         if prediction == 0:
             st.write('Le billet est authentique.')
         else:
             st.write('Le billet est un faux.')
+        st.write(f'Probabilité d\'obtention d\'un vrai billet : {proba[0]:.2f}')
+        st.write(f'Probabilité d\'obtention d\'un faux billet : {proba[1]:.2f}')
 
 if __name__ == '__main__':
     main()
