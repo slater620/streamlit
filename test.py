e5786b6463f5922a6b63ed4bf7aa198040499a2f
slater620 <chendjouruben@gmail.com>
2024-02-28 05:43:27 +0100

    model2e

---
+++ test.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import joblib
+
+# Charger le modèle
+model = joblib.load('model_adaBoot.joblib')
+
+# Interface utilisateur
+st.title('Prédiction du nutri_score')
+
+# Entrée des caractéristiques
+product = st.text_input('nom du produit', value="")
+energy = st.number_input('Énergie pour 100g', value=0)
+saturated_fat = st.number_input('Matières grasses saturées pour 100g', value=0)
+sugars = st.number_input('Sucres pour 100g', value=0)
+fiber = st.number_input('Fibres pour 100g', value=0)
+proteins = st.number_input('Protéines pour 100g', value=0)
+salt = st.number_input('Sel pour 100g', value=0)
+
+# Prédiction
+if st.button('Prédire'):
+    features = [[energy, saturated_fat, sugars, fiber, proteins, salt]]
+    prediction = model.predict(features)
+    st.write('Le nutri_score de', product ', prediction[0])
