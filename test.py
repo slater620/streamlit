e34fbc23a4fc3b1c0440de128e92dd468dc68bc7
slater620 <81989373+slater620@users.noreply.github.com>
2024-02-28 10:45:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ st.title('Prédiction du nutri_score')
 product = st.text_input('nom du produit', value="")
 energy = st.number_input('Énergie pour 100g',step=1.,format="%.2f")
 saturated_fat = st.number_input('Matières grasses saturées pour 100g',step=1.,format="%.2f")
-sugars = st.number_input('Sucres pour 100g', value=0)
-fiber = st.number_input('Fibres pour 100g', value=0)
-proteins = st.number_input('Protéines pour 100g', value=0)
-salt = st.number_input('Sel pour 100g', value=0)
+sugars = st.number_input('Sucres pour 100g',step=1.,format="%.2f")
+fiber = st.number_input('Fibres pour 100g',step=1.,format="%.2f")
+proteins = st.number_input('Protéines pour 100g',step=1.,format="%.2f")
+salt = st.number_input('Sel pour 100g',step=1.,format="%.2f")
 
 # Prédiction
 if st.button('Prédire'):
